c66b3baa06004c0f75625f25ea6dde32948215d8
Kyle Tseng <ktseng122@gmail.com>
2021-03-19 16:02:33 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Week 2 Unit Test Problems
+
+## Usage
+
+1. Download and unzip the skeleton (`week-2-unit-test-problems-v2.zip`)
+2. Rename the folder to `firstname-lastname-week-2-unit-test-problems`. For
+   example, if your name is "Jane Doe", then name your folder
+   `jane-doe-week-2-unit-test-problems`
+3. `cd` into the folder and `npm install` to install dependencies
+4. `mocha` to run all specs
+5. Your objective is to implement the functions in `/problems` to pass all specs
+
+## Submission
+
+When you are ready to submit:
+
+1. delete the `node_modules` directory
+2. zip up your folder
+3. upload it
---
+++ problems/06-dupe-char-min-count.js
@@ -0,0 +1,21 @@
+/***********************************************************************
+Write a function `duplicateCharMinCount(string, minCount)` that will takes
+a string as an argument and returns an array of characters that show up
+at least `minCount` number of times. The string will have at least one
+character.
+
+Examples:
+
+duplicateCharMinCount("apple", 2) // ["p"]
+duplicateCharMinCount("banana", 2) // ["a", "n"]
+duplicateCharMinCount("What about a longer string?", 3) // ["a", "t", " "]
+***********************************************************************/
+
+function duplicateCharMinCount(string, minCount) {
+
+	// your code here
+
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = duplicateCharMinCount;
